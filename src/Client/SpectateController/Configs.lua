819027decf834a295c9631c10663e7afb576bbbb
Enjin008 <vegabilepogchamp@gmail.com>
2026-04-16 17:28:09 -0600

    feat(spectate): input keycode configs

---
+++ src/Client/SpectateController/Configs.lua
@@ -0,0 +1,8 @@
+--// src/Client/SpectateController/Configs.lua
+--// Config-only; no magic values elsewhere in the controller.
+
+return {
+	INPUT_NEXT_TARGET = Enum.KeyCode.E,
+	INPUT_PREVIOUS_TARGET = Enum.KeyCode.Q,
+	INPUT_CLEAR_TARGET = Enum.KeyCode.X,
+}
