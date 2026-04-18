1682ea15e2dad2e711f7d3ecaf595dbf43e4c1cb
Enjin008 <vegabilepogchamp@gmail.com>
2026-04-17 21:07:55 -0600

    feat(gun): slim ShootAction client to release-fired sfx

--- src/Client/GunController/Actions/ShootAction.lua
+++ src/Client/GunController/Actions/ShootAction.lua
@@ -1,9 +1,7 @@
-local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local SharedConfigs = require(ReplicatedStorage.Gun.Configs)
 local AnimationType = require(ReplicatedStorage.Animations.AnimationType)
 local AnimationProfile = require(ReplicatedStorage.Animations.AnimationProfile)
-local AnimationController = require(script.Parent.Parent.Parent.AnimationController)
 local SFXController = require(script.Parent.Parent.Parent.SFXController)
 
 local ShootAction = {}
@@ -11,18 +9,14 @@ local ShootAction = {}
 ShootAction.name = "Shoot"
 ShootAction.cooldown = SharedConfigs.ShootCooldown
 ShootAction.duration = SharedConfigs.ShootDuration
+
 do
-	local _profile = AnimationProfile.resolve("SmallPistol", SharedConfigs.AnimationProfiles, AnimationType.Shoot)
-	ShootAction.animationId = (_profile and _profile.id) or ""
+	local profile = AnimationProfile.resolve("SmallPistol", SharedConfigs.AnimationProfiles, AnimationType.Shoot)
+	ShootAction.animationId = (profile and profile.id) or ""
 end
 
+--// Invoked from the release callback in GunController. Animation is already playing via the chain.
 function ShootAction.clientExecute(_state, _directionVector)
-	local character = Players.LocalPlayer.Character
-	if not character then
-		warn("[ShootAction] clientExecute: no character")
-		return
-	end
-	AnimationController.play(character, ShootAction.animationId)
 	SFXController.playUI(SharedConfigs.ShootSoundId)
 end
 
